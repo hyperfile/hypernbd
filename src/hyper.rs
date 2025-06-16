583b03527fe0fad62a8a4a8d4b22f0e3d75416b8
daiyy <daiyiyang@hotmail.com>
2025-06-16 06:22:50 +0000

    bulk code update / impl read and write

---
+++ src/hyper.rs
@@ -0,0 +1,76 @@
+use std::io::Result;
+use std::io::SeekFrom;
+use std::sync::Arc;
+use log::debug;
+use tokio::sync::RwLock;
+use tokio::io::{AsyncSeekExt, AsyncReadExt, AsyncWriteExt};
+use tokio::runtime::Runtime;
+use aws_sdk_s3::Client;
+use hyperfile::file::tokio_wrapper::HyperFileTokio;
+use hyperfile::file::flags::FileFlags;
+use hyperfile::file::mode::FileMode;
+
+pub(crate) struct HyperNbd<'a> {
+    uri: String,
+    rt: Runtime,
+    client: Client,
+    file: Arc<RwLock<HyperFileTokio<'a>>>,
+}
+
+impl<'a: 'static> HyperNbd<'a> {
+    pub(crate) fn open(uri: &str, readonly: bool) -> Result<Self> {
+        debug!("open back device: {}", uri);
+        let rt = tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+
+        let (file, client) = rt.block_on(async move {
+            let config = aws_config::load_from_env().await;
+            let client = aws_sdk_s3::Client::new(&config);
+
+            let flags = if readonly {
+                FileFlags::rdonly()
+            } else {
+                FileFlags::rdwr()
+            };
+            let mode = FileMode::default_file();
+            (HyperFileTokio::open_or_create_with_default_opt(&client, uri, flags, mode).await, client)
+        });
+
+        Ok(Self {
+            uri: uri.to_owned(),
+            rt,
+            client,
+            file: Arc::new(RwLock::new(file?)),
+        })
+    }
+
+    pub(crate) fn get_volume_size(&self) -> Result<i64> {
+        let file = self.file.clone();
+        let res = self.rt.handle().block_on(async {
+            file.read().await.metadata().await
+        });
+        res.map(|stat| stat.st_size as i64)
+    }
+
+    pub(crate) fn read(&self, offset: u64, buf: &mut [u8]) -> Result<()> {
+        let file = self.file.clone();
+        let res = self.rt.handle().block_on(async {
+            let mut lock = file.write().await;
+            let _ = lock.seek(SeekFrom::Start(offset)).await?;
+            lock.read_exact(buf).await
+        });
+        res.map(|_read_size| ())
+    }
+
+    pub(crate) fn write(&self, offset: u64, buf: &[u8]) -> Result<()> {
+        let file = self.file.clone();
+        let res = self.rt.handle().block_on(async {
+            let mut lock = file.write().await;
+            let _ = lock.seek(SeekFrom::Start(offset)).await?;
+            lock.write_all(buf).await
+        });
+        res.map(|_write_size| ())
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,18 @@
+use std::sync::OnceLock;
 use nbdkit::*;
+use log::debug;
 
-struct HyperNbd {
-}
+mod hyper;
 
-impl HyperNbd {
-    fn new() -> Self {
-        Self {}
-    }
-}
+use hyper::HyperNbd;
 
-impl Server for HyperNbd {
+static BACKEND_URI: OnceLock<String> = OnceLock::new();
+
+impl<'a: 'static> Server for HyperNbd<'a> {
+    // required
     fn get_size(&self) -> Result<i64> {
-        todo!();
+        debug!("get_size -");
+        Ok(self.get_volume_size()?)
     }
 
     fn name() -> &'static str {
@@ -19,12 +20,58 @@ impl Server for HyperNbd {
     }
 
     fn open(readonly: bool) -> Result<Box<dyn Server>> {
-        Ok(Box::new(HyperNbd::new()))
+        debug!("open - readonly: {}", readonly);
+        let Some(uri) = BACKEND_URI.get() else {
+            return Err(Error::new(libc::EINVAL, "failed to get backend uri"));
+        };
+        Ok(Box::new(HyperNbd::open(uri, readonly)?))
     }
 
     fn read_at(&self, buf: &mut [u8], offset: u64) -> Result<()> {
-        todo!();
+        debug!("read_at - offset: {}, len: {}", offset, buf.len());
+        Ok(self.read(offset, buf)?)
+    }
+
+    // overwrite provided
+    fn is_rotational(&self) -> Result<bool> {
+        debug!("is_rotational - set false");
+        Ok(false)
+    }
+
+    fn load() {
+        env_logger::init();
+        debug!("load -");
+    }
+
+    fn unload() {
+        debug!("unload -");
+    }
+
+    fn dump_plugin() {
+        debug!("dump plugin -");
+    }
+
+    fn config(key: &str, value: &str) -> Result<()> {
+        debug!("config - key: {}, value: {}", key, value);
+        if key == "backend_uri" {
+            // TODO: validate uri
+            let res = BACKEND_URI.set(value.to_string());
+            if res.is_err() {
+                return Err(Error::new(libc::EINVAL, "failed to set backend uri from command line input"));
+            }
+        }
+        Ok(())
+    }
+
+    fn config_complete() -> Result<()> {
+        debug!("config_complete -");
+        Ok(())
+    }
+
+    fn write_at(&self, buf: &[u8], offset: u64, flags: Flags) -> Result<()> {
+        debug!("write_at - offset: {}, len: {}, flags: {}", offset, buf.len(), flags.bits());
+        Ok(self.write(offset, buf)?)
     }
 }
 
-plugin!(HyperNbd {});
+plugin!(HyperNbd {is_rotational, load, unload, dump_plugin, config, config_complete, write_at});
